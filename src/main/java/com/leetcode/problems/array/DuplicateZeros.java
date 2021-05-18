1cb74f0693823fc22cbaca6894eb19325a252e9f
e.taghizade@asan.gov.az <e.taghizade@asan.gov.az>
2021-05-18 10:44:10 +0400

    Duplicate Zeros problem solved

--- src/main/java/com/leetcode/problems/array/DuplicateZeros.java
+++ src/main/java/com/leetcode/problems/array/DuplicateZeros.java
@@ -1,15 +1,9 @@
 package com.leetcode.problems.array;
 
-import java.util.Arrays;
-
 public class DuplicateZeros {
-    public static void main(String[] args) {
-        duplicateZeros(new int[]{1,0,2,3,0,4,5,0});
-    }
-
-    public static void duplicateZeros(int[] arr) {
+    public void duplicateZeros(int[] arr) {
         int countOfZeros = 0;
-        for (int j : arr) if (j == 0) countOfZeros++;
+        for (int num : arr) if (num == 0) countOfZeros++;
         int len = arr.length;
         int[] duplicated = new int[len + countOfZeros];
         int j = 0;
@@ -17,7 +11,7 @@ public class DuplicateZeros {
             if (arr[i] == 0) duplicated[j] = duplicated[j++ + 1] = 0;
             else duplicated[j] = arr[i];
             j++;
+            arr[i] = duplicated[i];
         }
-        System.out.println(Arrays.toString(duplicated));
     }
 }
