1bc12282598daf15ceeca9cd84efa149f1b2feaa
elvintaghizade14 <e.taghizade@asan.gov.az>
2021-05-26 10:21:43 +0400

    binary search implemented

---
+++ src/main/java/com/leetcode/binary_search/BinarySearch.java
@@ -0,0 +1,16 @@
+package com.leetcode.binary_search;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left <= right) {
+            mid = right - left;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+}
