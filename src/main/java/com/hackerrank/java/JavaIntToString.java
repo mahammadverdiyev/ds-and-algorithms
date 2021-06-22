d486fe436b7578dac83bf709ee6df49a733aeaf4
elvintaghizade <elvintaghiyev184@gmail.com>
2021-06-22 20:15:38 +0400

    "Java Int to String" solved

---
+++ src/main/java/com/hackerrank/java/JavaIntToString.java
@@ -0,0 +1,18 @@
+package com.hackerrank.java;
+
+import java.util.Scanner;
+
+public class JavaIntToString {
+    public static void main(String[] args) {
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            in.close();
+            String s = String.valueOf(n);
+            if (n == Integer.parseInt(s)) System.out.println("Good job");
+            else System.out.println("Wrong answer.");
+        } catch (Exception e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+}
