e05c6701c26c48fbdfc0b763c56bbd3190b979c7
elvintaghizade14 <e.taghizade@asan.gov.az>
2021-06-22 14:21:48 +0400

    "Welcome to Java" solved

--- src/main/java/com/hackerrank/java/WelcomeToJava.java
+++ src/main/java/com/hackerrank/java/WelcomeToJava.java
@@ -1,6 +1,6 @@
 package com.hackerrank.java;
 
-public class WelcomeToJavaApp {
+public class WelcomeToJava {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
