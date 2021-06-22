8367028fa7e84b65a0c79bf78c10c726eb273d1f
elvintaghizade14 <e.taghizade@asan.gov.az>
2021-06-22 15:14:38 +0400

    "Java Output Formatting" solved

---
+++ src/main/java/com/hackerrank/java/JavaOutputFormatting.java
@@ -0,0 +1,16 @@
+package com.hackerrank.java;
+
+import java.util.Scanner;
+
+public class JavaOutputFormatting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            final String s1 = sc.next();
+            final int x = sc.nextInt();
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
+    }
+}
