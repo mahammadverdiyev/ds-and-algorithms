dde227eea203cb39f95d84631af9feb90c0e6d54
elvintaghizade14 <e.taghizade@asan.gov.az>
2021-06-24 15:51:03 +0400

    "Java Substring Comparisons" should be implemented

---
+++ src/main/java/com/hackerrank/java/JavaSubstringComparisons.java
@@ -0,0 +1,16 @@
+package com.hackerrank.java;
+
+public class JavaSubstringComparisons {
+    public static void main(String[] args) {
+        System.out.println(getSmallestAndLargest("welcometojava", 3));
+    }
+
+    public static String getSmallestAndLargest(final String s, final int k) {
+        String smallest = "";
+        String largest = "";
+        final int len = s.length();
+
+
+        return smallest + "\n" + largest;
+    }
+}
