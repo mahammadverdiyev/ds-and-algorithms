cd11b4af160e645fdcba51f82f2d313d33dd6812
elvintaghizade14 <e.taghizade@asan.gov.az>
2021-06-29 11:02:22 +0400

    "Java Substring Comparisons" solved

--- src/main/java/com/hackerrank/java/JavaSubstringComparisons.java
+++ src/main/java/com/hackerrank/java/JavaSubstringComparisons.java
@@ -1,16 +1,14 @@
 package com.hackerrank.java;
 
 public class JavaSubstringComparisons {
-    public static void main(String[] args) {
-        System.out.println(getSmallestAndLargest("welcometojava", 3));
-    }
-
     public static String getSmallestAndLargest(final String s, final int k) {
-        String smallest = "";
-        String largest = "";
-        final int len = s.length();
-
-
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+        for (int i = 1; i < s.length() - k + 1; i++) {
+            String sub = s.substring(i, i + k);
+            if (smallest.compareTo(sub) > 0) smallest = sub;
+            if (largest.compareTo(sub) < 0) largest = sub;
+        }
         return smallest + "\n" + largest;
     }
 }
